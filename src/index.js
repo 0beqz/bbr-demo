f033d0af7a10493f8b0c6f93cc3e0b4d0ea41928
User <none>
2022-04-20 19:49:53 +0200

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,61 +1,81 @@
 import './style/main.css'
 import * as THREE from 'three'
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
+import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader";
+import BillboardReflection from "./BillboardReflection.module"
 
-/**
- * Sizes
- */
-const sizes = {}
-sizes.width = window.innerWidth
-sizes.height = window.innerHeight
-
-window.addEventListener('resize', () =>
-{
-    // Save sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
-
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
+const resize = () => {
+    camera.aspect = window.innerWidth / window.innerHeight
     camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+}
+window.addEventListener('resize', resize)
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-})
-
-/**
- * Environnements
- */
-// Scene
 const scene = new THREE.Scene()
 
-// Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.z = 3
+// load env map
+new RGBELoader().load("/env.hdr", tex => {
+    const pmremGenerator = new THREE.PMREMGenerator(renderer)
+    pmremGenerator.compileEquirectangularShader()
+    const envMap = pmremGenerator.fromEquirectangular(tex).texture
+
+    scene.environment = scene.background = envMap
+});
+
+
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+camera.position.set(0, 5, 10)
 scene.add(camera)
 
-// Test
-const cube = new THREE.Mesh(new THREE.BoxBufferGeometry(1, 1, 1), new THREE.MeshNormalMaterial())
-scene.add(cube)
-
-// Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector('.webgl')
 })
 renderer.setPixelRatio(window.devicePixelRatio)
-renderer.setSize(sizes.width, sizes.height)
+resize();
 
-/**
- * Loop
- */
-const loop = () =>
-{
-    // Update
-    cube.rotation.y += 0.01
 
-    // Render
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+
+const textureLoader = new THREE.TextureLoader()
+
+const billboardMesh = new THREE.Mesh(new THREE.PlaneBufferGeometry(), new THREE.MeshBasicMaterial({
+    map: textureLoader.load("/rubik.png"), // from https://www.flaticon.com/free-icons/cube
+    transparent: true,
+    side: THREE.DoubleSide
+}))
+billboardMesh.scale.setScalar(4)
+billboardMesh.position.y = 5
+scene.add(billboardMesh)
+
+
+const ground = new THREE.Mesh(new THREE.PlaneBufferGeometry(64, 64), new THREE.MeshStandardMaterial({
+    roughness: 2,
+    metalness: 1,
+    normalScale: new THREE.Vector2(0.1, 0.1),
+    normalMap: textureLoader.load("/nrm.jpg"),
+    roughnessMap: textureLoader.load("/rgh.png")
+}))
+ground.material.roughnessMap.wrapS = ground.material.roughnessMap.wrapT = ground.material.normalMap.wrapS = ground.material.normalMap.wrapT = THREE.RepeatWrapping
+ground.material.roughnessMap.repeat.setScalar(4)
+ground.material.normalMap.repeat.setScalar(4)
+ground.rotation.x = -Math.PI / 2
+scene.add(ground)
+
+
+// enable reflections
+
+let billboardReflection = new BillboardReflection()
+
+billboardReflection.create(billboardMesh)
+ground.material.onBeforeCompile = shader => billboardReflection.enableReflection(shader)
+
+const loop = () => {
+    controls.update();
     renderer.render(scene, camera)
-
-    // Keep looping
     window.requestAnimationFrame(loop)
 }
+
 loop()
